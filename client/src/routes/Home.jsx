a4f8ec32687cef0e11855904d961bfab310615e4
rakesh121272 <420samyaprahlad@gmail.com>
2024-09-07 02:17:15 +0530

    Update Home.jsx

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     TabTitle("Home - Shema");
 
     useEffect(() => {
-        axios.get("https://shema-backend.vercel.app/api/items")
+        axios.get("https://e-shopkart-2.onrender.com/api/items")
             .then(res => setFeaturedItems(res.data))
             .catch(err => console.log(err))
 
